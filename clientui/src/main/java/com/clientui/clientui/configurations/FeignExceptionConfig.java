dab5fd973ee9a2d9d22da07cdc0a35f5b02f712f
Chebil Bechir <Bechir.chebil@esprit.tn>
2021-05-30 00:00:02 +0100

    initial commit

---
+++ clientui/src/main/java/com/clientui/clientui/configurations/FeignExceptionConfig.java
@@ -0,0 +1,12 @@
+package com.clientui.clientui.configurations;
+
+import com.clientui.clientui.exceptions.CustomErrorDecoder;
+import org.springframework.context.annotation.Bean;
+
+public class FeignExceptionConfig {
+
+    @Bean
+    public CustomErrorDecoder mCustomErrorDecoder(){
+        return new CustomErrorDecoder();
+    }
+}
---
+++ clientui/src/main/java/com/clientui/clientui/exceptions/CustomErrorDecoder.java
@@ -0,0 +1,18 @@
+package com.clientui.clientui.exceptions;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String s, Response response) {
+
+        if (response.status() == 404) {
+            return new ProductNotFoundException("Produit non trouve");
+        }
+        return null;
+    }
+}
